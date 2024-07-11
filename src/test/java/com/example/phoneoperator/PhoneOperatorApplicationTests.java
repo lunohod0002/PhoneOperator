3f37f3451e92706a421838b2af6b102dcc3817ad
George <lunohod0002@gmail.com>
2024-07-11 10:53:29 +0300

    init

---
+++ src/test/java/com/example/phoneoperator/PhoneOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phoneoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
